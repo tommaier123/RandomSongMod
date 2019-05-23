7ba82e086e2e65c943c4eefa575768ff5de6ec47
Nova_Max <tom.maier@abwesend.de>
2019-05-24 00:03:09 +0200

    V1.1.0 Initial Commit
    V1.1.0 Initial Commit

---
+++ RandomButton.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using Synth.mods.utils;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using System.IO;
+
+public class RandomButton : MonoBehaviour
+{
+
+    public static RandomButton s_instance;
+
+    private Action<GameObject> uiCallback;
+    private Action<int> selectedCallback;
+    private List<TrackData> loadedTracks;
+
+    public GameObject wrap;
+    public GameObject canvas;
+    public Button button;
+    private bool canvasSet = false;
+
+    public static Action<GameObject> UICallback
+    {
+        get
+        {
+            return s_instance.uiCallback;
+        }
+
+        set
+        {
+            s_instance.uiCallback = value;
+        }
+    }
+
+    public static Action<int> SelectedCallback
+    {
+        get
+        {
+            return s_instance.selectedCallback;
+        }
+
+        set
+        {
+            s_instance.selectedCallback = value;
+        }
+    }
+
+    public static List<TrackData> LoadedTracks
+    {
+        get
+        {
+            return s_instance.loadedTracks;
+        }
+
+        set
+        {
+            s_instance.loadedTracks = value;
+        }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (s_instance != null)
+        {
+            Destroy(this);
+        }
+
+        s_instance = this;
+        DontDestroyOnLoad(this);
+
+        button.onClick.AddListener(PlayRandom);
+        Hide();
+    }
+
+    void Start()
+    {
+        if (s_instance == null) { return; }
+
+    }
+
+    public static void Show()
+    {
+        if (s_instance == null) { return; }
+        s_instance.wrap.SetActive(true);
+    }
+
+    public static void Hide()
+    {
+        if (s_instance == null) { return; }
+        s_instance.wrap.SetActive(false);
+    }
+
+    public static void InitCanvasVRTK()
+    {
+        if (!s_instance.canvasSet && s_instance.uiCallback != null)
+        {
+            s_instance.canvasSet = true;
+            s_instance.uiCallback(s_instance.canvas);
+        }
+    }
+
+    public void PlayRandom()
+    {
+
+        if (SelectedCallback != null && LoadedTracks != null)
+        {
+            var temp = UnityEngine.Random.Range(0, LoadedTracks.Count);
+            SelectedCallback(temp);
+            log("random" + temp.ToString() + ", " + LoadedTracks.Count.ToString() + Environment.NewLine);
+        }
+    }
+
+    public static void DestroyRandomButton()
+    {
+        if (s_instance == null) { return; }
+
+        Destroy(s_instance);
+    }
+
+    public void log(string str)
+    {
+        //get file path
+        var dataPath = Application.dataPath;
+        var filePath = dataPath.Substring(0, dataPath.LastIndexOf('/')) + "/Novalog.txt";
+
+        //write
+        using (var streamWriter = new StreamWriter(filePath, true))
+        {
+            streamWriter.Write(str);
+        }
+    }
+}
---
+++ RandomScript.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using UMod;
+using Synth.mods.utils;
+using Synth.mods.events;
+using Synth.mods.info;
+using System.Collections.Generic;
+using Synth.mods.interactions;
+using System;
+
+public class RandomScript : ModScript, ISynthRidersEvents, ISynthRidersInfo, ISynthRidersInteractions {
+
+	public override void OnModLoaded()
+	{
+        GameObject obj = ModAssets.Instantiate<GameObject>("RandomButton_pre");
+	}
+
+    public override void OnModUnload()
+    {		
+        RandomButton.DestroyRandomButton();
+	}
+
+    public void OnRoomLoaded()
+    {
+        RandomButton.Show();
+    }
+
+    public void OnRoomUnloaded()
+    {
+        RandomButton.Hide();
+    }
+
+    public void OnGameStageLoaded(TrackData trackData)
+    {
+
+    }
+
+    public void OnGameStageUnloaded()
+    {
+  
+    }	
+
+    public void OnScoreStageLoaded()
+    {
+
+    }
+
+    public void OnScoreStageUnloaded()
+    {
+
+    }
+
+    public void OnNoteFail(PointsData pointsData)
+    {
+
+    }
+
+    public void OnPointScored(PointsData pointsData)
+    {
+
+    }
+
+    public void OnSongFinished(SongFinishedData songFinishedData)
+    {
+
+    }
+
+    public void OnSongFailed(TrackData trackData)
+    {
+
+    }
+
+    public void SetLoadedTracks(List<TrackData> tracks)
+    {
+        RandomButton.LoadedTracks = tracks;
+    }
+
+    public void SetLoadedStages(List<StageData> stages)
+    {
+
+    }
+
+    public void SetUserSelectedColors(Color leftHandColor, Color rightHandColor, Color oneHandSpecialColor, Color bothHandSpecialColor)
+    {
+
+    }
+
+    public void SetUICanvasCallback(Action<GameObject> callback)
+    {
+        RandomButton.UICallback = callback;
+        RandomButton.InitCanvasVRTK();
+    }
+
+    public void SetGameOverCallback(Action callback)
+    {
+
+    }
+
+    public void SetPlayTrackCallback(Action<int, int, int> callback)
+    {
+        
+    }
+
+    public void SetCurrentSongSelected(int CurrentSong)
+    {
+
+    }
+
+    public void SetSelectedTrackCallback(Action<int> callback)
+    {
+        RandomButton.SelectedCallback = callback;
+    }
+
+    public void SetRefreshCallback(Action<Action, bool> callback)
+    {
+        
+    }
+
+    public void SetFilterTrackCallback(Action<List<string>, Action, bool> callback)
+    {
+        
+    }
+}
